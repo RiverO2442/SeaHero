69b9e760b927d3643a0bc2edbc7812aac0c75102
RiverO2442 <nguyem.anhhao.840@gmail.com>
2026-05-26 11:40:05 +0000

    fix: add missing ToastProvider import in main.tsx
    - ToastProvider was wrapped around App but import was missing
    - Fixes runtime ReferenceError on load

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { ToastProvider } from "./hooks/useToast.tsx";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ToastProvider><App /></ToastProvider>
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </StrictMode>,
 );
